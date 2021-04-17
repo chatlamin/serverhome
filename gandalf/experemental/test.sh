40230d7960491a904ff410246515e7b9558747e6
chatlamin <viktorap@inbox.ru>
2021-04-17 16:17:29 +0300

    fix

--- gandalf/experemental/test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Elevate privileges
-#[ $UID -eq 0 ] || exec sudo --preserve-env=VERSION bash "$0" "$@"
-
-DOCKER_HOST=$(hostname | awk -F '.' '{print $2"."$3}')
-
-echo $DOCKER_HOST
