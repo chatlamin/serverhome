f1716f7dc83951bf78eb86db704f7ee8f1938b19
chatlamin <viktorap@inbox.ru>
2021-04-20 21:10:03 +0300

    add grafana

---
+++ gandalf/containers/active/grafana/grafana/run.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+#---------------------------------------------------------------------
+# Settings
+#---------------------------------------------------------------------
+
+source settings-personal.sh
+source ../../settings/settings-common.sh
+
+#--------------------------------------------------------------------
+# End settings
+#--------------------------------------------------------------------
+
+# https://github.com/alexanderfefelov/scripts/blob/master/install/ops/install-docker.sh
+# Elevate privileges
+[ $UID -eq 0 ] || exec sudo bash "$0" "$@"
+
+docker run \
+    --name $CONTAINER_NAME \
+    --hostname $CONTAINER_NAME.$DOCKER_HOST_DOMEN \
+    --detach \
+    --restart unless-stopped \
+    --volume /etc/localtime:/etc/localtime:ro \
+    --volume /etc/timezone:/etc/timezone:ro \
+    --volume $CONTAINER_NAME-conf:/etc/grafana \
+    --env GF_DATABASE_TYPE=$DB_CONNECTION \
+    --env GF_DATABASE_HOST=$DB_HOST \
+    --env GF_DATABASE_NAME=$DB_DATABASE \
+    --env GF_DATABASE_USER=$DB_USERNAME \
+    --env GF_DATABASE_PASSWORD=$DB_PASSWORD \
+    --publish 3000:3000 \
+    $IMAGE_TARGET
+
+docker logs --follow $CONTAINER_NAME
