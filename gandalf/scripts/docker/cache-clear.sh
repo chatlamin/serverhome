8f76e93d7b30592cd5619ccfdb3f96c4aac6820b
chatlamin <viktorap@inbox.ru>
2021-04-20 21:11:06 +0300

    Скрипт удаления кэша докера

---
+++ gandalf/scripts/docker/cache-clear.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# https://github.com/alexanderfefelov/scripts/blob/master/install/ops/install-docker.sh
+# Elevate privileges
+[ $UID -eq 0 ] || exec sudo bash "$0" "$@"
+
+docker builder prune --all
