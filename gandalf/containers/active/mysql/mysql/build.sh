9749611640538247bacf1b9b45cc330ac45c4824
chatlamin <viktorap@inbox.ru>
2021-04-20 20:28:27 +0300

    fix link

--- gandalf/containers/active/mysql/mysql/build.sh
+++ gandalf/containers/active/mysql/mysql/build.sh
@@ -11,7 +11,7 @@ source ../../settings/settings-common.sh
 # End settings
 #--------------------------------------------------------------------
 
-# https://github.com/alexanderfefelov/scripts/blob/ba800d0fbd66a335fbdfdfc8fa5683ffb3a0411a/install/ops/install-docker.sh
+# https://github.com/alexanderfefelov/scripts/blob/master/install/ops/install-docker.sh
 # Elevate privileges
 [ $UID -eq 0 ] || exec sudo bash "$0" "$@"
 
