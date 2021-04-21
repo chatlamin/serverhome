a10403667743ba4ce20c372c597414f15e4b5f5f
chatlamin <viktorap@inbox.ru>
2021-04-21 20:01:06 +0300

    add bookstack

---
+++ gandalf/containers/active/bookstack/bookstack/README.md
@@ -0,0 +1,8 @@
+# Ссылки на проект
+https://www.bookstackapp.com/
+
+https://github.com/BookStackApp/BookStack
+
+https://github.com/linuxserver/docker-bookstack
+
+https://hub.docker.com/r/linuxserver/bookstack
---
+++ gandalf/containers/active/bookstack/bookstack/settings-personal.sh
@@ -0,0 +1,35 @@
+#---------------------------------------------------------------------
+# Settings
+#---------------------------------------------------------------------
+
+##
+# Название проекта
+export NAME=bookstack
+# Репозиторий
+EXPLORE=linuxserver/bookstack
+# Тэг образа старый
+TAG_OLD=version-v21.04.2
+# Тэг образа новый
+TAG_NEW=version-v21.04.2
+
+##
+# URL приложения
+APP_URL=http://bookstack.serverhome.home:65001
+# ip / dns базы данных
+DB_HOST=mysql.serverhome.home
+# Порт базы данных
+DB_PORT=3306
+# Пользователь базы данных
+DB_USERNAME=bookstack
+# Пароль базы данных
+DB_PASSWORD=di1ieweesaekoF
+# Имя базы данных
+DB_DATABASE=bookstack
+# Имя контейнера базы данных
+DB_CONTAINER_NAME=mysql
+# Пароль от root
+MYSQL_ROOT_PASSWORD=Dae2fiiChohng0
+
+#--------------------------------------------------------------------
+# End settings
+#--------------------------------------------------------------------
