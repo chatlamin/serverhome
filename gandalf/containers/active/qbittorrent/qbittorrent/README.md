73be45ef51295ed6f6aa343a091f54222bad287e
chatlamin <viktorap@inbox.ru>
2021-04-21 21:50:17 +0300

    add qbittorrent

---
+++ gandalf/containers/active/qbittorrent/qbittorrent/README.md
@@ -0,0 +1,10 @@
+# Ссылки на проект
+https://www.qbittorrent.org/
+
+https://github.com/qbittorrent/qBittorrent
+
+https://github.com/linuxserver/docker-qbittorrent
+
+https://hub.docker.com/r/linuxserver/qbittorrent
+
+Логин admin пароль il8oquee9eHo6m
---
+++ gandalf/containers/active/qbittorrent/qbittorrent/run.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+#---------------------------------------------------------------------
+# Settings
+#---------------------------------------------------------------------
+
+source settings-personal.sh
+source ../../settings/settings-common.sh
+
+#--------------------------------------------------------------------
+# End settings
+#--------------------------------------------------------------------
+
+# https://github.com/alexanderfefelov/scripts/blob/master/install/ops/install-docker.sh
+# Elevate privileges
+[ $UID -eq 0 ] || exec sudo bash "$0" "$@"
+
+docker run \
+    --name $CONTAINER_NAME \
+    --hostname $CONTAINER_NAME.$DOCKER_HOST_DOMEN \
+    --detach \
+    --restart unless-stopped \
+    --volume $CONTAINER_NAME-conf:/config \
+    --volume $CONTAINER_NAME-data:/downloads \
+    --env TZ=Europe/Moscow \
+    --env WEBUI_PORT=65004 \
+    --publish 65004:65004 \
+    --publish 6881:6881 \
+    --publish 6881:6881/udp \
+    $IMAGE_TARGET
+
+docker logs --follow $CONTAINER_NAME
+
+#   --env WEBUI_PORT=65004 \
+#    --publish 65004:65004 \
+#    --publish 6881:6881 \
+#    --publish 6881:6881/udp \
