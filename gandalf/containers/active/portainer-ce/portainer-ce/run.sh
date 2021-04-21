46293d50292cc0acdd54453c1929a7f3016bcf66
chatlamin <viktorap@inbox.ru>
2021-04-21 09:33:36 +0300

    add portainer-ce

---
+++ gandalf/containers/active/portainer-ce/portainer-ce/run.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+#---------------------------------------------------------------------
+# Settings
+#---------------------------------------------------------------------
+
+source settings-personal.sh
+source ../../settings/settings-common.sh
+
+#--------------------------------------------------------------------
+# End settings
+#--------------------------------------------------------------------
+
+# https://github.com/alexanderfefelov/scripts/blob/master/install/ops/install-docker.sh
+# Elevate privileges
+[ $UID -eq 0 ] || exec sudo bash "$0" "$@"
+
+docker run \
+    --name $CONTAINER_NAME \
+    --hostname $CONTAINER_NAME.$DOCKER_HOST_DOMEN \
+    --detach \
+    --restart unless-stopped \
+    --volume /etc/localtime:/etc/localtime:ro \
+    --volume /etc/timezone:/etc/timezone:ro \
+    --volume /var/run/docker.sock:/var/run/docker.sock \
+    --volume $CONTAINER_NAME-pass:/password \
+    --volume $CONTAINER_NAME-data:/data \
+    --publish 9000:9000 \
+    $IMAGE_TARGET \
+    --admin-password-file '/password/portainer-ce-password'
+
+docker logs --follow $CONTAINER_NAME
