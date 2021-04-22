f346861bd0c5d8b03531ae8869232fe84bd1917f
chatlamin <viktorap@inbox.ru>
2021-04-22 10:38:22 +0300

    add homeassistant

---
+++ gandalf/containers/active/homeassistant/homeassistant/README.md
@@ -0,0 +1,8 @@
+# Ссылки на проект
+https://www.home-assistant.io/
+
+https://github.com/home-assistant/core
+
+https://github.com/home-assistant/docker
+
+https://hub.docker.com/r/homeassistant/home-assistant
---
+++ gandalf/containers/active/homeassistant/homeassistant/settings-personal.sh
@@ -0,0 +1,33 @@
+#---------------------------------------------------------------------
+# Settings
+#---------------------------------------------------------------------
+
+##
+# Название проекта
+export NAME=homeassistant
+# Репозиторий
+EXPLORE=homeassistant/home-assistant
+# Тэг образа старый
+TAG_OLD=2021.4.6
+# Тэг образа новый
+TAG_NEW=2021.4.6
+
+##
+# Тип базы данных
+DB_CONNECTION=mysql
+# ip / dns базы данных
+DB_HOST=mysql.serverhome.home
+# Имя базы данных
+DB_DATABASE=homeassistant
+# Пользователь базы данных
+DB_USERNAME=homeassistant
+# Пароль базы данных
+DB_PASSWORD=chah6viCaapeeD
+# Имя контейнера базы данных
+DB_CONTAINER_NAME=mysql
+# Пароль от root
+MYSQL_ROOT_PASSWORD=Dae2fiiChohng0
+
+#--------------------------------------------------------------------
+# End settings
+#--------------------------------------------------------------------
