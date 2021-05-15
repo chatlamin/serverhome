18b795864bd47fbab2c6c8e317277e953e3b5b86
chatlamin <viktorap@inbox.ru>
2021-05-15 19:53:23 +0300

    add healthchecks

---
+++ pippin/containers/active/healthchecks/healthchecks/README.md
@@ -0,0 +1,8 @@
+# Ссылки на проект
+https://healthchecks.io/
+
+https://github.com/healthchecks/healthchecks
+
+https://github.com/linuxserver/docker-healthchecks
+
+https://hub.docker.com/r/linuxserver/healthchecks
---
+++ pippin/containers/active/healthchecks/healthchecks/run.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+#---------------------------------------------------------------------
+# Settings
+#---------------------------------------------------------------------
+
+source settings-personal.sh
+source ../../settings/settings-common.sh
+
+#--------------------------------------------------------------------
+# End settings
+#--------------------------------------------------------------------
+
+# https://github.com/alexanderfefelov/scripts/blob/master/install/ops/install-docker.sh
+# Elevate privileges
+[ $UID -eq 0 ] || exec sudo bash "$0" "$@"
+
+docker run \
+    --log-driver=loki \
+    --log-opt loki-url=$LOKI_URL \
+    --log-opt loki-retries=10 \
+    --log-opt loki-batch-size=102400 \
+    --log-opt no-file=false \
+    --log-opt keep-file=true \
+    --log-opt max-size=5m \
+    --log-opt max-file=3 \
+    --name $CONTAINER_NAME \
+    --hostname $CONTAINER_NAME.$DOCKER_HOST_DOMEN \
+    --detach \
+    --restart always \
+    --volume /etc/localtime:/etc/localtime:ro \
+    --volume /etc/timezone:/etc/timezone:ro \
+    --volume $CONTAINER_NAME-conf:/config \
+    --env SITE_ROOT=http://$CONTAINER_NAME.$DOCKER_HOST_DOMEN \
+    --env SITE_NAME=$CONTAINER_NAME.$DOCKER_HOST_DOMEN \
+    --env ALLOWED_HOSTS="*" \
+    --env SUPERUSER_EMAIL=$SUPERUSER_EMAIL \
+    --env SUPERUSER_PASSWORD=$SUPERUSER_PASSWORD \
+    --env REGISTRATION_OPEN=False \
+    --env SECRET_KEY=$SECRET_KEY \
+    --env DB=$DB_CONNECTION \
+    --env DB_HOST=$DB_HOST \
+    --env DB_NAME=$DB_DATABASE \
+    --env DB_USER=$DB_USERNAME \
+    --env DB_PASSWORD=$DB_PASSWORD \
+    --publish 8000:8000 \
+    $IMAGE_TARGET
+
+docker logs --follow $CONTAINER_NAME
