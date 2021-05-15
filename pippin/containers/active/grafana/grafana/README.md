5403b3fbf4d53c597dde8d998663a0003432eb66
chatlamin <viktorap@inbox.ru>
2021-05-15 17:12:23 +0300

    add firefly-iii

---
+++ pippin/containers/active/grafana/grafana/README.md
@@ -0,0 +1,6 @@
+# Ссылки на проект
+https://grafana.com/
+
+https://github.com/grafana/grafana
+
+https://hub.docker.com/r/grafana/grafana
---
+++ pippin/containers/active/grafana/grafana/settings-personal.sh
@@ -0,0 +1,39 @@
+#---------------------------------------------------------------------
+# Settings
+#---------------------------------------------------------------------
+
+##
+# Название проекта
+export NAME=grafana
+# Репозиторий
+EXPLORE=grafana/grafana
+# Тэг образа старый
+TAG_OLD=7.5.4
+# Тэг образа новый
+TAG_NEW=7.5.5
+
+##
+# Тип базы данных
+DB_CONNECTION=mysql
+# ip / dns базы данных
+DB_HOST=mysql.serverhome.home
+# Имя базы данных
+DB_DATABASE=grafana
+# Пользователь базы данных
+DB_USERNAME=grafana
+# Пароль базы данных
+DB_PASSWORD=shohg5Sei3igh3
+# Имя контейнера базы данных
+DB_CONTAINER_NAME=mysql
+# Пароль от root
+MYSQL_ROOT_PASSWORD=Dae2fiiChohng0
+# Логин дэфолтного администратора
+ADMIN_USER=admin
+# Пароль дэфолтного администратора
+ADMIN_PASS=Thee2veih0hoo4
+# Путь к дэфолтному дэшборду
+DASHBOARDS_DEFAULT_HOME_DASHBOARD_PATH=/etc/grafana/provisioning/dashboards/common/telegraf-system.json
+
+#--------------------------------------------------------------------
+# End settings
+#--------------------------------------------------------------------
