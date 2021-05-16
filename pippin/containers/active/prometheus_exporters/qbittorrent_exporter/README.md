24c80a15d1dbb6c94d548a8098f37271f241af5d
chatlamin <viktorap@inbox.ru>
2021-05-16 17:55:14 +0300

    add exporters

---
+++ pippin/containers/active/prometheus_exporters/qbittorrent_exporter/README.md
@@ -0,0 +1,2 @@
+# Ссылки на проект
+https://github.com/esanchezm/prometheus-qbittorrent-exporter
---
+++ pippin/containers/active/prometheus_exporters/qbittorrent_exporter/run.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+#---------------------------------------------------------------------
+# Settings
+#---------------------------------------------------------------------
+
+source settings-personal.sh
+source ../../settings/settings-common.sh
+
+#--------------------------------------------------------------------
+# End settings
+#--------------------------------------------------------------------
+
+# https://github.com/alexanderfefelov/scripts/blob/master/install/ops/install-docker.sh
+# Elevate privileges
+[ $UID -eq 0 ] || exec sudo bash "$0" "$@"
+
+docker run \
+    --log-driver=loki \
+    --log-opt loki-url=$LOKI_URL \
+    --log-opt loki-retries=10 \
+    --log-opt loki-batch-size=102400 \
+    --log-opt no-file=false \
+    --log-opt keep-file=true \
+    --log-opt max-size=5m \
+    --log-opt max-file=3 \
+    --name $CONTAINER_NAME \
+    --hostname $CONTAINER_NAME.$DOCKER_HOST_DOMEN \
+    --detach \
+    --restart unless-stopped \
+    --volume /etc/localtime:/etc/localtime:ro \
+    --volume /etc/timezone:/etc/timezone:ro \
+    --env QBITTORRENT_HOST=$QBITTORRENT_HOST \
+    --env QBITTORRENT_PORT=$QBITTORRENT_PORT \
+    --publish 65101:8000 \
+    $IMAGE_TARGET
+
+docker logs --follow $CONTAINER_NAME
