3fc95388065dcffbd9cf98318bf9acc71c40897f
chatlamin <viktorap@inbox.ru>
2021-05-20 20:50:28 +0300

    add curator

---
+++ gandalf/containers/active/curator/curator/README.md
@@ -0,0 +1,7 @@
+# Ссылки на проект
+
+https://www.elastic.co/guide/en/elasticsearch/client/curator/current/index.html
+
+https://github.com/elastic/curator
+
+https://hub.docker.com/r/bitnami/elasticsearch-curator
---
+++ gandalf/containers/active/curator/curator/run.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+#---------------------------------------------------------------------
+# Settings
+#---------------------------------------------------------------------
+
+source settings-personal.sh
+source ../../settings/settings-common.sh
+
+#--------------------------------------------------------------------
+# End settings
+#--------------------------------------------------------------------
+
+# https://github.com/alexanderfefelov/scripts/blob/master/install/ops/install-docker.sh
+# Elevate privileges
+[ $UID -eq 0 ] || exec sudo bash "$0" "$@"
+
+docker run \
+    --rm \
+    --name $CONTAINER_NAME \
+    --hostname $CONTAINER_NAME.$DOCKER_HOST_DOMEN \
+    --volume /etc/localtime:/etc/localtime:ro \
+    --volume /etc/timezone:/etc/timezone:ro \
+    $IMAGE_TARGET \
+    --config /etc/curator/curator.yml /etc/curator/action.yml
+
+docker logs --follow $CONTAINER_NAME
