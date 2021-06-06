61e7e40394d035b41c4c5a8ff8d4ec992b4ca442
chatlamin <viktorap@inbox.ru>
2021-06-06 11:37:20 +0300

    add scrutiny

---
+++ pippin/containers/active/scrutiny/scrutiny/README.md
@@ -0,0 +1,8 @@
+# Ссылки на проект
+https://github.com/AnalogJ/scrutiny
+
+https://hub.docker.com/r/analogj/scrutiny
+
+https://hub.docker.com/r/hotio/scrutiny
+
+https://hub.docker.com/r/linuxserver/scrutiny
---
+++ pippin/containers/active/scrutiny/scrutiny/run.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+#---------------------------------------------------------------------
+# Settings
+#---------------------------------------------------------------------
+
+source settings-personal.sh
+source ../../settings/settings-common.sh
+
+#--------------------------------------------------------------------
+# End settings
+#--------------------------------------------------------------------
+
+# https://github.com/alexanderfefelov/scripts/blob/master/install/ops/install-docker.sh
+# Elevate privileges
+[ $UID -eq 0 ] || exec sudo bash "$0" "$@"
+
+docker run \
+    --name $CONTAINER_NAME \
+    --hostname $CONTAINER_NAME.$DOCKER_HOST_DOMEN \
+    --detach \
+    --restart unless-stopped \
+    --volume $CONTAINER_NAME-conf:/config \
+    --volume $CONTAINER_NAME-data:/data \
+    --env TZ="Europe/Moscow" \
+    --env MODE="web" \
+    --publish 65012:8080 \
+    $IMAGE_TARGET
+
+docker logs --follow $CONTAINER_NAME
