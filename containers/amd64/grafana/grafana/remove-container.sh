c8e0848aaea02614d65c116ae9a21eb46a3a44a3
chatlamin <viktorap@inbox.ru>
2021-08-05 09:44:26 +0300

    restructuring

---
+++ containers/amd64/grafana/grafana/remove-container.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+#---------------------------------------------------------------------
+# Settings
+#---------------------------------------------------------------------
+
+source settings-personal.sh
+source ../../settings/settings-common.sh
+
+#--------------------------------------------------------------------
+# End settings
+#--------------------------------------------------------------------
+
+# https://github.com/alexanderfefelov/scripts/blob/master/install/ops/install-docker.sh
+# Elevate privileges
+[ $UID -eq 0 ] || exec sudo bash "$0" "$@"
+
+docker stop $CONTAINER_NAME
+docker rm -f $CONTAINER_NAME
