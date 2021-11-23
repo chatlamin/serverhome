8fac99cee52b6fe8010b7925de5a7e55481a927b
chatlamin <viktorap@inbox.ru>
2021-11-23 17:32:04 +0300

    add zabbix_agent2

---
+++ containers/zabbix/zabbix_agent2/README.md
@@ -0,0 +1,36 @@
+# zabbix_agent2
+Hostname: `zabbix_agent2.serverhome.home`
+
+## Supported architectures
+| Architectures | Support |
+| :------------ | :------ |
+| amd64         | +       |
+| arm64         | ?       |
+| armhf         | ?       |
+
+## Keys
+Link: `zabbix_agent2.serverhome.home:65031`
+
+Login: `no authentication support`
+
+Password: `no authentication support`
+
+## Database
+Type: `mysql-8`
+
+Name: `zabbix`
+
+Username: `zabbix`
+
+Password: `Tiec2eyooL9Eem`
+
+## References to the original project
+Website: https://www.zabbix.com/ru
+
+Documentation: https://www.zabbix.com/ru/manuals
+
+Repository github: https://github.com/zabbix/zabbix
+
+Repository docker github: https://github.com/zabbix/zabbix-docker/tree/5.4/Dockerfiles/agent2/ubuntu
+
+Repository docker: https://hub.docker.com/r/zabbix/zabbix-agent2
---
+++ containers/zabbix/zabbix_agent2/run.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+#---------------------------------------------------------------------
+# Settings
+#---------------------------------------------------------------------
+
+source settings-personal.sh
+source ../../settings/settings-common.sh
+
+#--------------------------------------------------------------------
+# End settings
+#--------------------------------------------------------------------
+
+# https://github.com/alexanderfefelov/scripts/blob/master/install/ops/install-docker.sh
+# Elevate privileges
+[ $UID -eq 0 ] || exec sudo bash "$0" "$@"
+
+docker run \
+    --name $CONTAINER_NAME \
+    --hostname $CONTAINER_NAME.$DOCKER_HOST_DOMEN \
+    --detach \
+    --restart unless-stopped \
+    --privileged \
+    --user 1997:$(stat -c '%g' /var/run/docker.sock) \
+    --volume /etc/localtime:/etc/localtime:ro \
+    --volume /etc/timezone:/etc/timezone:ro \
+    --volume /var/run/docker.sock:/var/run/docker.sock \
+    --volume $CONTAINER_NAME-conf:/etc/zabbix \
+    --env ZBX_HOSTNAME=$HOST_NAME_HOST \
+    --env ZBX_SERVER_HOST=zabbix_server.serverhome.home \
+    --env ZBX_SERVER_PORT=10051 \
+    --publish 65031:10050 \
+    $IMAGE_TARGET
+
+docker logs --follow $CONTAINER_NAME
