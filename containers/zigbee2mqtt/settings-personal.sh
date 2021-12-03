aa78b24e9315d27c572f0ce4face67936d14709d
chatlamin <viktorap@inbox.ru>
2021-12-03 21:10:27 +0300

    update zigbee2mqtt 1.22.1

--- containers/zigbee2mqtt/settings-personal.sh
+++ containers/zigbee2mqtt/settings-personal.sh
@@ -8,9 +8,9 @@ export NAME=zigbee2mqtt
 # Репозиторий
 EXPLORE=koenkk/zigbee2mqtt
 # Тэг образа старый
-TAG_OLD=1.18.2
+TAG_OLD=1.21.2
 # Тэг образа новый
-TAG_NEW=1.21.2
+TAG_NEW=1.22.1
 
 ##
 # Имя zigbee устройства
