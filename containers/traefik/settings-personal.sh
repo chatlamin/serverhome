7ad17b14e38672849a6d570843eed323fc6da4af
chatlamin <viktorap@inbox.ru>
2021-12-25 11:04:31 +0300

    update traefik v2.5.6

--- containers/traefik/settings-personal.sh
+++ containers/traefik/settings-personal.sh
@@ -8,9 +8,9 @@ export NAME=traefik
 # Репозиторий
 EXPLORE=traefik
 # Тэг образа старый
-TAG_OLD=v2.5.4
+TAG_OLD=v2.5.5
 # Тэг образа новый
-TAG_NEW=v2.5.5
+TAG_NEW=v2.5.6
 
 #--------------------------------------------------------------------
 # End settings
