87535d9f4fd7ca41570581781d484500fe4cbb2e
chatlamin <viktorap@inbox.ru>
2022-01-15 12:56:38 +0300

    update plex version-1.25.3.5409-f11334058

--- containers/plex/settings-personal.sh
+++ containers/plex/settings-personal.sh
@@ -8,9 +8,9 @@ export NAME=plex
 # Репозиторий
 EXPLORE=linuxserver/plex
 # Тэг образа старый
-TAG_OLD=version-1.25.1.5286-34f965be8
+TAG_OLD=version-1.25.2.5319-c43dc0277
 # Тэг образа новый
-TAG_NEW=version-1.25.2.5319-c43dc0277
+TAG_NEW=version-1.25.3.5409-f11334058
 
 #--------------------------------------------------------------------
 # End settings
