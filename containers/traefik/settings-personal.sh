4e7c5e58bd81ed4e181e6024baafd6332d404e95
chatlamin <viktorap@inbox.ru>
2022-01-23 19:18:30 +0300

    возврат traefik на стабильную версию v2.5.7

--- containers/traefik/settings-personal.sh
+++ containers/traefik/settings-personal.sh
@@ -8,9 +8,9 @@ export NAME=traefik
 # Репозиторий
 EXPLORE=traefik
 # Тэг образа старый
-TAG_OLD=v2.5.6
+TAG_OLD=v2.6
 # Тэг образа новый
-TAG_NEW=v2.6
+TAG_NEW=v2.5.7
 
 #--------------------------------------------------------------------
 # End settings
