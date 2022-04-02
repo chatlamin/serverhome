4ab57d364d2a55ea16c6db912e6060036fd303e0
chatlamin <viktorap@inbox.ru>
2022-04-02 12:03:00 +0300

    update home-assistant 2022.3.8

--- containers/home-assistant/settings-personal.sh
+++ containers/home-assistant/settings-personal.sh
@@ -8,15 +8,15 @@ export NAME=home-assistant
 # Репозиторий
 EXPLORE=homeassistant/home-assistant
 # Тэг образа старый
-TAG_OLD=2022.2.9
+TAG_OLD=2022.3.5
 # Тэг образа новый
-TAG_NEW=2022.3.5
+TAG_NEW=2022.3.8
 
 ##
 # Тип базы данных
 DB_CONNECTION=mysql
 # ip / dns базы данных
-DB_HOST=mysql-8.serverhome.home
+DB_HOST=mysql-8-master.serverhome.home
 # Имя базы данных
 DB_DATABASE=home_assistant
 # Пользователь базы данных
@@ -24,7 +24,7 @@ DB_USERNAME=home_assistant
 # Пароль базы данных
 DB_PASSWORD=chah6viCaapeeD
 # Имя контейнера базы данных
-DB_CONTAINER_NAME=mysql-8
+DB_CONTAINER_NAME=mysql-8-master
 # Пароль от root
 MYSQL_ROOT_PASSWORD=Dae2fiiChohng0
 
