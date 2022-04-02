15fe5aaa6252ff043270d3565786a70d7de08bc1
chatlamin <viktorap@inbox.ru>
2022-04-02 14:23:08 +0300

    add filebrowser

---
+++ containers/filebrowser/README.md
@@ -0,0 +1,41 @@
+# filebrowser
+
+Hostname: `filebrowser.serverhome.home`
+
+## Supported architectures
+
+| Architectures | Support |
+| :------------ | :------ |
+| amd64         | +       |
+| arm64         | ?       |
+| armhf         | ?       |
+
+## Keys
+
+Link: http://filebrowser.serverhome.home:65110
+
+Login: `admin`
+
+Password: `admin`
+
+## Database
+
+Type: `?`
+
+Name: `?`
+
+Username: `?`
+
+Password: `?`
+
+## References to the original project
+
+Website: https://filebrowser.org/
+
+Documentation: https://filebrowser.org/
+
+Repository github: https://github.com/filebrowser/filebrowser
+
+Repository docker github: https://github.com/filebrowser/filebrowser/blob/master/Dockerfile.s6
+
+Repository docker: https://hub.docker.com/r/filebrowser/filebrowser
---
+++ containers/filebrowser/run.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+#---------------------------------------------------------------------
+# Settings
+#---------------------------------------------------------------------
+
+source settings-personal.sh
+source ../settings/settings-common.sh
+
+#--------------------------------------------------------------------
+# End settings
+#--------------------------------------------------------------------
+
+# https://github.com/alexanderfefelov/scripts/blob/master/install/ops/install-docker.sh
+# Elevate privileges
+[ $UID -eq 0 ] || exec sudo bash "$0" "$@"
+
+docker run \
+    --env-file public.env \
+    --name $CONTAINER_NAME \
+    --hostname $CONTAINER_NAME.$DOCKER_HOST_DOMEN \
+    --detach \
+    --restart unless-stopped \
+    --volume /etc/localtime:/etc/localtime:ro \
+    --volume /etc/timezone:/etc/timezone:ro \
+    --volume $CONTAINER_NAME-data:/database \
+    --volume $CONTAINER_NAME-conf:/config \
+    --volume $CONTAINER_NAME-srv:/srv \
+    --publish 65110:80 \
+    $HEALTHCHECK_SETTINGS \
+    $IMAGE_TARGET
+
+docker logs --follow $CONTAINER_NAME
