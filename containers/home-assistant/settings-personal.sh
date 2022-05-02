fe3dffbcabc1727edceaa2a461237c51f1cc5941
chatlamin <viktorap@inbox.ru>
2022-05-02 13:12:42 +0300

    update home-assistant 2022.4.7

--- containers/home-assistant/settings-personal.sh
+++ containers/home-assistant/settings-personal.sh
@@ -8,9 +8,9 @@ export NAME=home-assistant
 # Репозиторий
 EXPLORE=homeassistant/home-assistant
 # Тэг образа старый
-TAG_OLD=2022.4.1
+TAG_OLD=2022.4.4
 # Тэг образа новый
-TAG_NEW=2022.4.4
+TAG_NEW=2022.4.7
 
 ##
 # Тип базы данных
