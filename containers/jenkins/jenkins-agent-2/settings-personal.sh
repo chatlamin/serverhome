94f216da10fd6c9844754c586e634613ced8d7b9
chatlamin <viktorap@inbox.ru>
2022-05-02 13:19:22 +0300

    update jenkins-agent-2 4.13-2

--- containers/jenkins/jenkins-agent-2/settings-personal.sh
+++ containers/jenkins/jenkins-agent-2/settings-personal.sh
@@ -8,9 +8,9 @@ export NAME=jenkins-agent-2
 # Репозиторий
 EXPLORE=jenkins/inbound-agent
 # Тэг образа старый
-TAG_OLD=4.10-2
+TAG_OLD=4.13-1
 # Тэг образа новый
-TAG_NEW=4.13-1
+TAG_NEW=4.13-2
 
 #--------------------------------------------------------------------
 # End settings
