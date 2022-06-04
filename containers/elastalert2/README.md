b7477378e19d7e0b0d53bf8ed7a91d352e3d9b7e
chatlamin <viktorap@inbox.ru>
2022-06-04 10:08:42 +0300

    add alestalert2

---
+++ containers/elastalert2/README.md
@@ -0,0 +1,41 @@
+# elastalert2
+
+Hostname: `elastalert2.serverhome.home`
+
+## Supported architectures
+
+| Architectures | Support |
+| :------------ | :------ |
+| amd64         | +       |
+| arm64         | ?       |
+| armhf         | ?       |
+
+## Keys
+
+Link: `elastalert2.serverhome.home`
+
+Login: `?`
+
+Password: `?`
+
+## Database
+
+Type: `elasticsearch`
+
+Name: `elastalert2`
+
+Username: `-`
+
+Password: `-`
+
+## References to the original project
+
+Website: https://github.com/jertel/elastalert2
+
+Documentation: https://elastalert2.readthedocs.io/en/latest/index.html
+
+Repository github: https://github.com/jertel/elastalert2
+
+Repository docker github: https://github.com/jertel/elastalert2/blob/master/Dockerfile
+
+Repository docker: https://hub.docker.com/r/jertel/elastalert2
---
+++ containers/elastalert2/run.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+#---------------------------------------------------------------------
+# Settings
+#---------------------------------------------------------------------
+
+source settings-personal.sh
+source ../settings/settings-common.sh
+
+#--------------------------------------------------------------------
+# End settings
+#--------------------------------------------------------------------
+
+# https://github.com/alexanderfefelov/scripts/blob/master/install/ops/install-docker.sh
+# Elevate privileges
+[ $UID -eq 0 ] || exec sudo bash "$0" "$@"
+
+docker run \
+    --env-file public.env \
+    --name $CONTAINER_NAME \
+    --hostname $CONTAINER_NAME.$DOCKER_HOST_DOMEN \
+    --detach \
+    --restart unless-stopped \
+    --volume $CONTAINER_NAME-conf:/opt/elastalert \
+    $IMAGE_TARGET \
+    --verbose
+
+docker logs --follow $CONTAINER_NAME
