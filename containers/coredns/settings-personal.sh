792f7a2d29c28ec3778baae8adb37e2c5afe92f8
chatlamin <viktorap@inbox.ru>
2022-07-02 15:58:48 +0300

    update coredns 1.9.1

--- containers/coredns/settings-personal.sh
+++ containers/coredns/settings-personal.sh
@@ -8,9 +8,9 @@ export NAME=coredns
 # Репозиторий
 EXPLORE=coredns/coredns
 # Тэг образа старый
-TAG_OLD=1.9.0
+TAG_OLD=1.9.1
 # Тэг образа новый
-TAG_NEW=1.9.1
+TAG_NEW=1.9.3
 
 #--------------------------------------------------------------------
 # End settings
