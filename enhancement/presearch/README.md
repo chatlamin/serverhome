5ca158f2d5a0c8fde76c4e39a03e8ac42be0b7fa
chatlamin <viktorap@inbox.ru>
2022-08-01 19:29:32 +0300

    work presearch

---
+++ enhancement/presearch/README.md
@@ -0,0 +1,41 @@
+# presearch
+
+Hostname: `presearch.serverhome.home`
+
+## Supported architectures
+
+| Architectures | Support |
+| :------------ | :------ |
+| amd64         | +       |
+| arm64         | ?       |
+| armhf         | ?       |
+
+## Keys
+
+Link: `presearch.serverhome.home`
+
+Login: `-`
+
+Password: `-`
+
+## Database
+
+Type: `-`
+
+Name: `-`
+
+Username: `-`
+
+Password: `-`
+
+## References to the original project
+
+Website: https://presearch.com/
+
+Documentation: https://docs.presearch.org/
+
+Repository github: ?
+
+Repository docker github: ?
+
+Repository docker: https://hub.docker.com/r/presearch/node
---
+++ enhancement/presearch/run.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+#---------------------------------------------------------------------
+# Settings
+#---------------------------------------------------------------------
+
+source settings-personal.sh
+source ../settings/settings-common.sh
+
+#--------------------------------------------------------------------
+# End settings
+#--------------------------------------------------------------------
+
+# https://github.com/alexanderfefelov/scripts/blob/master/install/ops/install-docker.sh
+# Elevate privileges
+[ $UID -eq 0 ] || exec sudo bash "$0" "$@"
+
+docker run \
+    --env-file private.env \
+    --env-file public.env \
+    --name $CONTAINER_NAME \
+    --hostname $CONTAINER_NAME.$DOCKER_HOST_DOMEN \
+    --detach \
+    --restart unless-stopped \
+    --volume $CONTAINER_NAME-data:/app/node \
+    $IMAGE_TARGET
+
+docker logs --follow $CONTAINER_NAME
