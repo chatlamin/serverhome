230a2373a3e61ee6c6712e32c551fcf9a8154247
chatlamin <viktorap@inbox.ru>
2022-09-09 17:35:05 +0300

    work nginx-proxy-manager

---
+++ enhancement/nginx-proxy-manager/README.md
@@ -0,0 +1,41 @@
+# nginx-proxy-manager
+
+Hostname: `nginx-proxy-manager.serverhome.home`
+
+## Supported architectures
+
+| Architectures | Support |
+| :------------ | :------ |
+| amd64         | +       |
+| arm64         | ?       |
+| armhf         | ?       |
+
+## Keys
+
+Link: http://nginx-proxy-manager.serverhome.home:65117/
+
+Login: `?`
+
+Password: `?`
+
+## Database
+
+Type: `mysql-8`
+
+Name: `nginx_proxy_manager`
+
+Username: `nginx_proxy_manager`
+
+Password: `quie6phai6Chel`
+
+## References to the original project
+
+Website: https://nginxproxymanager.com/
+
+Documentation: https://nginxproxymanager.com/guide/
+
+Repository github: https://github.com/NginxProxyManager/nginx-proxy-manager
+
+Repository docker github: https://github.com/NginxProxyManager/nginx-proxy-manager/tree/develop/docker
+
+Repository docker: https://hub.docker.com/r/jc21/nginx-proxy-manager
---
+++ enhancement/nginx-proxy-manager/run.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+#---------------------------------------------------------------------
+# Settings
+#---------------------------------------------------------------------
+
+source settings-personal.sh
+source ../settings/settings-common.sh
+
+#--------------------------------------------------------------------
+# End settings
+#--------------------------------------------------------------------
+
+# https://github.com/alexanderfefelov/scripts/blob/master/install/ops/install-docker.sh
+# Elevate privileges
+[ $UID -eq 0 ] || exec sudo bash "$0" "$@"
+
+docker run \
+    --env-file public.env \
+    --name $CONTAINER_NAME \
+    --hostname $CONTAINER_NAME.$DOCKER_HOST_DOMEN \
+    --detach \
+    --restart unless-stopped \
+    --volume /etc/localtime:/etc/localtime:ro \
+    --volume /etc/timezone:/etc/timezone:ro \
+    --volume $CONTAINER_NAME-data:/data \
+    --volume $CONTAINER_NAME-cert:/etc/letsencrypt \
+    --publish 65117:81 \
+    --publish 80:80 \
+    --publish 443:443 \
+    $HEALTHCHECK_SETTINGS \
+    $IMAGE_TARGET
+
+docker logs --follow $CONTAINER_NAME
