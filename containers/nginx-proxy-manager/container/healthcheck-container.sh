ff66db5e55838cd501f95ed43536b9ac4813cf77
chatlamin <viktorap@inbox.ru>
2022-09-09 19:22:36 +0300

    add nginx-proxy-manager

--- containers/nginx-proxy-manager/container/healthcheck-container.sh
+++ containers/nginx-proxy-manager/container/healthcheck-container.sh
@@ -7,4 +7,7 @@
 # End settings
 #--------------------------------------------------------------------
 
+# https://nginxproxymanager.com/advanced-config/#docker-healthcheck
+/bin/check-health
+
 curl --head --fail --silent --max-time 2 http://localhost:81 > /dev/null
