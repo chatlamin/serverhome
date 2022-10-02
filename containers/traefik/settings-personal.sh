17ca2c99d39a50a682f65de7467428ef4e7a0665
chatlamin <viktorap@inbox.ru>
2022-10-02 11:52:31 +0300

    update traefik:v2.8.8

--- containers/traefik/settings-personal.sh
+++ containers/traefik/settings-personal.sh
@@ -8,9 +8,9 @@ export NAME=traefik
 # Репозиторий
 EXPLORE=traefik
 # Тэг образа старый
-TAG_OLD=v2.8.0
+TAG_OLD=v2.8.5
 # Тэг образа новый
-TAG_NEW=v2.8.5
+TAG_NEW=v2.8.8
 
 #--------------------------------------------------------------------
 # End settings
