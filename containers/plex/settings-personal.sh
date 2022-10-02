717cc0e846199ddae48493fe580d49dc3d04669a
chatlamin <viktorap@inbox.ru>
2022-10-02 11:43:32 +0300

    update plex:version-1.29.0.6244-819d3678c

--- containers/plex/settings-personal.sh
+++ containers/plex/settings-personal.sh
@@ -8,9 +8,9 @@ export NAME=plex
 # Репозиторий
 EXPLORE=linuxserver/plex
 # Тэг образа старый
-TAG_OLD=version-1.28.2.6106-44a5bbd28
+TAG_OLD=version-1.28.2.6151-914ddd2b3
 # Тэг образа новый
-TAG_NEW=version-1.28.2.6151-914ddd2b3
+TAG_NEW=version-1.29.0.6244-819d3678c
 
 #--------------------------------------------------------------------
 # End settings
