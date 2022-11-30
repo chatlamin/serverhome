d4e2ed484aaa7778c24026dcec118bf8564b1822
chatlamin <viktorap@inbox.ru>
2022-11-30 21:27:29 +0300

    update plex:version-1.29.2.6364-6d72b0cf6

--- containers/plex/settings-personal.sh
+++ containers/plex/settings-personal.sh
@@ -8,9 +8,9 @@ export NAME=plex
 # Репозиторий
 EXPLORE=linuxserver/plex
 # Тэг образа старый
-TAG_OLD=version-1.29.0.6244-819d3678c
+TAG_OLD=version-1.29.1.6316-f4cdfea9c
 # Тэг образа новый
-TAG_NEW=version-1.29.1.6316-f4cdfea9c
+TAG_NEW=version-1.29.2.6364-6d72b0cf6
 
 #--------------------------------------------------------------------
 # End settings
