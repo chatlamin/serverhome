7ebf4a76dc7c66ec0d00afa4acad85684273e493
chatlamin <viktorap@inbox.ru>
2022-11-30 21:29:50 +0300

    update firefly-iii:version-5.7.15

--- containers/firefly-iii/settings-personal.sh
+++ containers/firefly-iii/settings-personal.sh
@@ -8,9 +8,9 @@ export NAME=firefly-iii
 # Репозиторий
 EXPLORE=fireflyiii/core
 # Тэг образа старый
-TAG_OLD=version-5.7.9
+TAG_OLD=version-5.7.12
 # Тэг образа новый
-TAG_NEW=version-5.7.12
+TAG_NEW=version-5.7.15
 
 # Пароль от root
 MYSQL_ROOT_PASSWORD=Dae2fiiChohng0
