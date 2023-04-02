85dfa2b3d24c07182b523b938c621780db1cd301
chatlamin <viktorap@inbox.ru>
2023-04-02 12:01:36 +0300

    update firefly-iii:version-6.0.6

--- containers/firefly-iii/settings-personal.sh
+++ containers/firefly-iii/settings-personal.sh
@@ -8,9 +8,9 @@ export NAME=firefly-iii
 # Репозиторий
 EXPLORE=fireflyiii/core
 # Тэг образа старый
-TAG_OLD=version-5.7.12
+TAG_OLD=version-5.7.15
 # Тэг образа новый
-TAG_NEW=version-5.7.15
+TAG_NEW=version-6.0.6
 
 # Пароль от root
 MYSQL_ROOT_PASSWORD=Dae2fiiChohng0
