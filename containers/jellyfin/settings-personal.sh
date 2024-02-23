736676367a7b4bb81a8fffbbda56fef1446e492a
chatlamin <viktorap@inbox.ru>
2024-02-23 14:34:12 +0300

    update jellyfin:10.8.13

--- containers/jellyfin/settings-personal.sh
+++ containers/jellyfin/settings-personal.sh
@@ -8,9 +8,9 @@ export NAME=jellyfin
 # Репозиторий
 EXPLORE=jellyfin/jellyfin
 # Тэг образа старый
-TAG_OLD=10.8.10
+TAG_OLD=10.8.11
 # Тэг образа новый
-TAG_NEW=10.8.11
+TAG_NEW=10.8.13
 
 #--------------------------------------------------------------------
 # End settings
