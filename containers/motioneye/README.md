011bde40e92f93e17f0fcebea12c5bcb69371862
chatlamin <viktorap@inbox.ru>
2024-02-25 18:47:17 +0300

    add motioneye

---
+++ containers/motioneye/README.md
@@ -0,0 +1,36 @@
+# motioneye
+Hostname: `motioneye.serverhome.home`
+
+## Supported architectures
+| Architectures | Support |
+| :------------ | :------ |
+| amd64         | +       |
+| arm64         | ?       |
+| armhf         | ?       |
+
+## Keys
+Link: http://motioneye.serverhome.home:65205
+
+Login: `admin`
+
+Password: `-`
+
+## Database
+Type: `-`
+
+Name: `-`
+
+Username: `-`
+
+Password: `-`
+
+## References to the original project
+Website: https://github.com/motioneye-project/motioneye
+
+Documentation: https://github.com/motioneye-project/motioneye/wiki
+
+Repository github: https://github.com/motioneye-project/motioneye
+
+Dockerfile: https://github.com/motioneye-project/motioneye/blob/main/docker/Dockerfile
+
+Repository docker: https://hub.docker.com/r/ccrisan/motioneye
---
+++ containers/motioneye/run.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+#---------------------------------------------------------------------
+# Settings
+#---------------------------------------------------------------------
+
+source settings-personal.sh
+source ../settings/settings-common.sh
+
+#--------------------------------------------------------------------
+# End settings
+#--------------------------------------------------------------------
+
+# https://github.com/alexanderfefelov/scripts/blob/master/install/ops/install-docker.sh
+# Elevate privileges
+[ $UID -eq 0 ] || exec sudo bash "$0" "$@"
+
+docker run \
+    --env-file private.env \
+    --env-file public.env \
+    --name $CONTAINER_NAME \
+    --hostname $CONTAINER_NAME.$DOCKER_HOST_DOMEN \
+    --detach \
+    --restart unless-stopped \
+    --volume /etc/localtime:/etc/localtime:ro \
+    --volume /etc/timezone:/etc/timezone:ro \
+    --volume $CONTAINER_NAME-data:/var/lib/motioneye \
+    --volume $CONTAINER_NAME-conf:/etc/motioneye \
+    --volume $CONTAINER_NAME-run:/var/run/motion \
+    --publish 65205:8765 \
+    $HEALTHCHECK_SETTINGS \
+    $IMAGE_TARGET
+
+docker logs --follow $CONTAINER_NAME
