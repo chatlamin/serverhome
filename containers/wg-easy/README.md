5d852453fb2a11584171a5f63eca53f027026719
chatlamin <viktorap@inbox.ru>
2024-09-15 12:19:54 +0300

    add wg-easy

---
+++ containers/wg-easy/README.md
@@ -0,0 +1,36 @@
+# wg-easy
+Hostname: `wg-easy.serverhome.home`
+
+## Supported architectures
+| Architectures | Support |
+| :------------ | :------ |
+| amd64         | +       |
+| arm64         | ?       |
+| armhf         | ?       |
+
+## Keys
+Link: http://wg-easy.serverhome.home:51821
+
+Login: `-`
+
+Password: generate https://bcrypt-generator.com/
+
+## Database
+Type: `-`
+
+Name: `-`
+
+Username: `-`
+
+Password: `-`
+
+## References to the original project
+Website: https://github.com/wg-easy/wg-easy
+
+Documentation: https://github.com/wg-easy/wg-easy/wiki
+
+Repository github: https://github.com/wg-easy/wg-easy
+
+Dockerfile: https://github.com/wg-easy/wg-easy/blob/master/Dockerfile
+
+Repository docker: https://github.com/wg-easy/wg-easy/pkgs/container/wg-easy
---
+++ containers/wg-easy/create.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+#---------------------------------------------------------------------
+# Settings
+#---------------------------------------------------------------------
+
+source settings-personal.sh
+source ../settings/settings-common.sh
+
+#--------------------------------------------------------------------
+# End settings
+#--------------------------------------------------------------------
+
+# https://github.com/alexanderfefelov/scripts/blob/master/install/ops/install-docker.sh
+# Elevate privileges
+[ $UID -eq 0 ] || exec sudo bash "$0" "$@"
+
+docker create \
+    --env-file private.env \
+    --env-file public.env \
+    --cap-add=NET_ADMIN \
+    --cap-add=SYS_MODULE \
+    --sysctl="net.ipv4.conf.all.src_valid_mark=1" \
+    --sysctl="net.ipv4.ip_forward=1" \
+    --name $CONTAINER_NAME \
+    --hostname $CONTAINER_NAME.$DOCKER_HOST_DOMEN \
+    --restart unless-stopped \
+    --volume /etc/localtime:/etc/localtime:ro \
+    --volume /etc/timezone:/etc/timezone:ro \
+    --volume $CONTAINER_NAME-conf:/etc/wireguard \
+    --publish 51820:51820/udp \
+    --pubkish 51821:51821 \
+    $HEALTHCHECK_SETTINGS \
+    $IMAGE_TARGET
